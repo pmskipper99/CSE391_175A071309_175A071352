945b963bc827fd850985744fae8ce87b95609da0
tiepnguyen6799 <tiepnt72@wru.vn>
2019-05-20 23:08:50 +0700

    indexmtcn

---
+++ indexmtcn/home mtcn/js/Hover.js
@@ -0,0 +1,91 @@
+
+$(function($) {
+                // di chuot vao menu
+                $('.menuparent1').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent1').removeClass('current');
+                    $('.menuchild1').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                // di chuot vao menu
+                $('.menuparent2').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent2').removeClass('current');
+                    $('.menuchild2').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                // di chuot vao menu
+                $('.menuparent3').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent3').removeClass('current');
+                    $('.menuchild3').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                // di chuot vao menu
+                $('.menuparent4').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent4').removeClass('current');
+                    $('.menuchild4').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                // di chuot vao menu
+                $('.menuparent5').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent5').removeClass('current');
+                    $('.menuchild5').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                // di chuot vao menu
+                $('.menuparent6').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent6').removeClass('current');
+                    $('.menuchild6').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                // di chuot vao menu
+                $('.menuparent7').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent7').removeClass('current');
+                    $('.menuchild7').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                 // di chuot vao menu
+                $('.menuparent8').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent8').removeClass('current');
+                    $('.menuchild8').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                 // di chuot vao menu
+                $('.menuparent9').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent9').removeClass('current');
+                    $('.menuchild9').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                 // di chuot vao menu
+                $('.menuparent10').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent10').removeClass('current');
+                    $('.menuchild10').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+                 // di chuot vao menu
+                $('.menuparent12').mouseover(function(){
+                    var id = $(this).attr('rel');
+                    $('.menuparent12').removeClass('current');
+                    $('.menuchild12').hide();
+                    $(this).addClass('current');
+                    $('#'+id).show();
+                });
+})
